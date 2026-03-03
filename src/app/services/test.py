b822db9a9e85386c17c76b129568ffe4201bce1c
Vincent112704 <112465584+Vincent112704@users.noreply.github.com>
2026-03-03 21:13:40 +0800

    corrected model output

--- src/app/services/test.py
+++ src/app/services/test.py
@@ -11,9 +11,12 @@ def model_interface1(email_text: str) -> str:
 
     print(res)
     if label == "LABEL_1" and score >= threshold:   
-        return "phishing"
+        return "Phishing"
     else:
         return "legitimate"
     
     
-    
+test1 = '''
+---------- Forwarded message --------- From: Atome <no-reply@service.atome.ph> Date: Tue, Mar 3, 2026 at 12:05 PM Subject: Transaction Confirmation: CBTL 1789 To: <tian2x04@gmail.com> Dear Vincent Paul, We are pleased to inform you that your payment of ₱195.00 for CBTL 1789 using QR Ph has been successfully processed. You can access your Loan Schedule Agreement via Atome App under Transaction Details once the transaction has been fully processed with the merchant. We look forward to serving you again in the future. Your friends at Atome Card Atome.ph <https://linkmessage.apaylater.com/ss/c/u001.9rMiIu5Qu4YP7GTVnc688ZkULyiolcyZ_x85FmQhcJo/4om/3aNvd_vERVKBibubQq13eA/h0/h001.r7kpirO4KT1xdEY_2ntRmioOSvamVkMXbQuNqyrTj5c> This email and any attachments are confidential and may also be privileged. If you are not the intended recipient, please delete all copies and notify support@atome.ph immediately Copyright 2026 © Atome. All rights reserved
+'''
+model_interface1(test1)
